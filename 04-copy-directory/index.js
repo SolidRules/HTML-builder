716c5fdc77d2b2503bc82f244e352c3fe17f9a35
SolidRules <amikheev1993@gmail.comm>
2022-11-07 01:01:05 +0300

    feat: complete task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+//Подключаем модули
+const fsPromis = require('fs/promises');
+const fs = require('fs');
+const path = require('path');
 
+async function copyDir() {
+    const sourceDirPath = path.join(__dirname, 'files');
+    const targetDirPath = path.join(__dirname, 'files-copy');
+    fsPromis.mkdir(targetDirPath, { recursive: true });
+    const sourceFileList = await fsPromis.readdir(sourceDirPath);
+    const targetFileList = await fsPromis.readdir(targetDirPath);
+    for (let file of targetFileList) {
+        const targetFilePath = path.join(targetDirPath, file);
+        fsPromis.unlink(targetFilePath);
+    }
+    for (let file of sourceFileList) {
+        const sourceFilePath = path.join(sourceDirPath, file);
+        const targetFilePath = path.join(targetDirPath, file);
+        const input = await fs.createReadStream(sourceFilePath, 'utf-8');
+        const output = await fs.createWriteStream(targetFilePath);
+        input.pipe(output);
+    }
+}
+
+copyDir();
